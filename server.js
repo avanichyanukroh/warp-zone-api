feb2367844dd1e3e731d6f7b1d297dbbbed23db8
Anawin Vanichyanukroh <avanichyanukroh@gmail.com>
2018-08-20 04:19:53 -0700

    added access control allow origin port 8000

--- server.js
+++ server.js
@@ -25,6 +25,16 @@ app.use(express.json());
 
 app.use(cors({origin: CLIENT_ORIGIN}));
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  if (req.method === 'OPTIONS') {
+    return res.send(204);
+  }
+  next();
+});
+
 const createAuthToken = (user) => {
 	return jwt.sign({user}, config.JWT_SECRET, {
 		subject: user.username,
